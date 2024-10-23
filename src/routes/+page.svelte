de6b95757a3b5b6b9e0da24b95dabe47c8c8bd51
Ruiqi Wu <s4107940@student.rmit.edu.au>
2024-10-24 00:19:49 +1100

    path

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -489,7 +489,6 @@
                 <LineLayer
                     layout={{ 'line-cap': 'round', 'line-join': 'round' }}
                     paint={{ 'line-color': 'purple', 'line-width': 3 }}
-                    beforeLayerType="symbol"
                 />
             </GeoJSON>
         {/if}
